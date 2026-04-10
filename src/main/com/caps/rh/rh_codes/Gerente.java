6f9e2ef592f1f22a6cd9424b194eefbb8789c915
Victor Hugo <211873799+Victor140113@users.noreply.github.com>
2026-04-10 11:47:10 -0300

    feat: Implementação base da Classe Gerente

--- src/main/com/caps/rh/rh_codes/Gerente.java
+++ src/main/com/caps/rh/rh_codes/Gerente.java
@@ -1,4 +1,21 @@
 package rh_codes;
-public class Gerente {
+public class Gerente extends Funcionario{
+
+	// Atributos Específicos
 	
+	private double bonusAnual;
+	
+	// Construtor
+	public Gerente(String nome, String cpf, double salario, double bonus) {
+		super(nome, cpf, salario);
+	}
+	
+	// Getters e Setters
+	public double getBonusAnual() {
+		return bonusAnual;
+	}
+
+	public void setBonusAnual(double bonusAnual) {
+		this.bonusAnual = bonusAnual;
+	}
 }
--- src/main/com/caps/rh/rh_codes/Main.java
+++ src/main/com/caps/rh/rh_codes/Main.java
@@ -1,5 +1,6 @@
 package rh_codes;
 public class Main {
+	
 	public static void main (String[] args){
         System.out.println("Testando Git + GitHub, quarto commit");
     }
